5c2de3ab119928d6668e8848f1ab17bc2c2588e1
ThigSchuch <thigschuch@gmail.com>
2024-10-22 14:08:23 -0300

    feat: Create and Update
    help of https://awstip.com/django-inline-formset-factory-with-examples-27576b915b5a

--- client/urls.py
+++ client/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from client.views import ClientDeleteView, ClientListView
+from client.views import (
+    ClientCreateView,
+    ClientDeleteView,
+    ClientListView,
+    ClientUpdateView,
+)
 
 app_name = "client"
 
 urlpatterns = [
     path("list/", ClientListView.as_view(), name="list_client"),
+    path("create/", ClientCreateView.as_view(), name="create_client"),
+    path("update/<int:pk>/", ClientUpdateView.as_view(), name="update_client"),
     path("delete/<int:pk>/", ClientDeleteView.as_view(), name="delete_client"),
 ]
